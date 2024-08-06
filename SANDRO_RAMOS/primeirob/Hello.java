0f1991d967860df60bec00d1b7056c37ff03fe44
Villuntos-Skafikis <prawn.staging-0x@icloud.com>
2024-08-06 20:18:44 -0300

    Primeiro commit aula 02

--- SANDRO_RAMOS/primeirob/Hello.java
+++ SANDRO_RAMOS/primeirob/Hello.java
@@ -2,6 +2,6 @@ package SANDRO_RAMOS.primeirob;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+        System.out.println("Hello World!");
     }
 }
