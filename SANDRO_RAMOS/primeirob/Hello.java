dadf2dd7091b3f5c3cf5e769f297405969df8560
Edilson <eagoncalves@minha.fag.edu.br>
2024-08-07 22:01:04 -0300

    teste4

--- SANDRO_RAMOS/primeirob/Hello.java
+++ SANDRO_RAMOS/primeirob/Hello.java
@@ -3,5 +3,19 @@ package SANDRO_RAMOS.primeirob;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("informe um valor");
+        int valorUm = leitor.nextint();
+
+        System.out.println("informe o valor dois");
+        int valorDois = leitor.nextint();
+
+        String valor = valorUm> valorDois ? "é maior" : "menor"
+
+        System.out.println
+       
     }
+
 }
