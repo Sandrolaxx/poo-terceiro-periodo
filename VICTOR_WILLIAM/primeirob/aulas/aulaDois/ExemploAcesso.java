78891f39eff99d15fdcbb96bdcb8d0c403a0b987
Villuntos-Skafikis <prawn.staging-0x@icloud.com>
2024-08-14 01:28:22 -0300

    criando classes - EstruturaControle - ExemploAcesso - Main - Vector

--- VICTOR_WILLIAM/primeirob/aulas/aulaDois/ExemploAcesso.java
+++ VICTOR_WILLIAM/primeirob/aulas/aulaDois/ExemploAcesso.java
@@ -1,5 +1,12 @@
 package VICTOR_WILLIAM.primeirob.aulas.aulaDois;
 
 public class ExemploAcesso {
+    // private eh o mais restritivo, so pode ser acessado dentro da propria classe
     private int idade;
+    // protected pode ser acessado por classes do mesmo pacote e classes filhas
+    protected int idadeDois = 20;
+    // protected final nao pode ser alterado, mas pode ser acessado por classes do mesmo pacote e classes filhas
+    protected final int VALOR_IMUTAVEL = 666;
+    // protected final static nao pode ser alterado, mas pode ser acessado por classes do mesmo pacote e classes filhas
+    protected final static int VALOR_IMUTAVEL_STATIC = 616;
 }
--- VICTOR_WILLIAM/primeirob/aulas/aulaDois/Main.java
+++ VICTOR_WILLIAM/primeirob/aulas/aulaDois/Main.java
@@ -2,6 +2,10 @@ package VICTOR_WILLIAM.primeirob.aulas.aulaDois;
 
 public class Main {
     public static void main(String[] args) {
+        ExemploAcesso exemploAcesso = new ExemploAcesso();
 
+        System.out.println(exemploAcesso.idadeDois);
+        System.out.println(exemploAcesso.VALOR_IMUTAVEL);
+        System.out.println(exemploAcesso.VALOR_IMUTAVEL_STATIC);
     }
 }
