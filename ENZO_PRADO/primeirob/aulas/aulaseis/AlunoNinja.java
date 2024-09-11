402493054145212520d4e994dd23bdade20231f9
Enzo Prado <142319794+Enzogpr@users.noreply.github.com>
2024-09-11 19:56:42 -0300

    Lista 5

---
+++ ENZO_PRADO/primeirob/aulas/aulaseis/AlunoNinja.java
@@ -0,0 +1,16 @@
+package ENZO_PRADO.primeirob.aulas.aulaseis;
+
+public class AlunoNinja extends Ninja {
+
+    String nomeTime;
+    String professor;
+
+
+
+
+@Override
+    void aprsentarse(){
+        System.out.println("Sou o aluno ninja ".concat(nome).concat("Do time: ".concat(nomeTime)));
+    }
+    
+}
