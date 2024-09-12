a71cdc46d72033fb863b9328f8e221edb5b31dad
Jffqueiroz <jffqueiroz@minha.fag.edu.br>
2024-09-11 22:10:41 -0300

    Lista Cinco

---
+++ JOAO_QUEIROZ/primeirob/aulas/aulacinco/ArmaTeste.java
@@ -0,0 +1,10 @@
+package primeirob.aulas.aulacinco;
+
+public class ArmaTeste {
+    public static  void main(String[] args) {
+        Arma fogo = new Arma();
+        fogo.nome = "Tiro";
+        fogo.tiro = 12;
+        System.out.println(fogo.nome);
+    }
+}
---
+++ JOAO_QUEIROZ/primeirob/aulas/aulacinco/Ninja.java
@@ -0,0 +1,24 @@
+package primeirob.aulas.aulacinco;
+
+public class Ninja {
+    String name;
+
+    int idade;
+
+    String Aldeia;
+
+    String cla;
+
+    String equipamento;
+
+    String nomeJutsu;
+
+    public void apresentarse() {
+        System.out.println("Olá meu nome é " + name + " da aldeia da " + Aldeia);
+    }
+
+    public void realizarJutsu() {
+        System.out.println("Realizando jutsu " + nomeJutsu + "!!");
+    }
+        
+}
