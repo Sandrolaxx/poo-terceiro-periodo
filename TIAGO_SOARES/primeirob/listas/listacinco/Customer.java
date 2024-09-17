0e97d5fc2ac8c46ff247850ae3b1e1af9758c6ea
tssoares1 <tssoares1@minha.fag.edu.br>
2024-09-17 19:27:47 -0300

    Lista 06 - Vendas bombando e caos no desenvolvimento

--- TIAGO_SOARES/primeirob/listas/listacinco/Customer.java
+++ TIAGO_SOARES/primeirob/listas/listacinco/Customer.java
@@ -2,12 +2,12 @@ package primeirob.listas.listacinco;
 
 public class Customer {
     String name;
-        Integer age;
-        String city;
-        String neighborhood;
-        String street;
+    Integer age;
+    String city;
+    String neighborhood;
+    String street;
 
-        void introduceYourself(){
-            System.out.println("(Customer) Hi, I'm ".concat(name).concat(". I have, ") + age + " years.\n");
-        }
+    void introduceYourself(){
+        System.out.println("(Customer) Hi, I'm ".concat(name).concat(". I have, ") + age + " years.\n");
+    }
 }
