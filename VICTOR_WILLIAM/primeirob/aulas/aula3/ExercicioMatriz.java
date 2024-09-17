53b79cebec0ef0b866a0003c872be958ff8eb442
Villuntos-Skafikis <prawn.staging-0x@icloud.com>
2024-09-17 19:51:31 -0300

    commit aula 3

---
+++ VICTOR_WILLIAM/primeirob/aulas/aula3/ExercicioMatriz.java
@@ -0,0 +1,12 @@
+package VICTOR_WILLIAM.primeirob.aulas.aula3;
+
+public class ExercicioMatriz {
+    public static void main(String[] args) {
+        int[][] matriz = new int[][]{
+                {31, 27, 04},
+                {46, 32, 95},
+                {29, 16, 56}
+        };
+    }
+
+}
