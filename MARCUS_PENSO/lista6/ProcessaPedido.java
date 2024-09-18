2dc913cd071248fe01fcad611c8ddd1635664572
marcusmode <mzpvanso@minha.fag.edu.br>
2024-09-18 20:47:30 -0300

    Lista6

---
+++ MARCUS_PENSO/lista6/ProcessaPedido.java
@@ -0,0 +1,12 @@
+package MARCUS_PENSO.lista6;
+import java.util.ArrayList;
+import java.util.Date;
+public class ProcessaPedido {
+    public Pedido processar(int id, Date dataCriacao, Cliente cliente, Vendedor vendedor, Loja loja, ArrayList<Item> itens) {
+        Date dataVencimentoReserva = new Date(dataCriacao.getTime() + 3 * 24 * 60 * 60 * 1000); // Reserva vence em 3 dias
+        return new Pedido(id, dataCriacao, null, dataVencimentoReserva, cliente, vendedor, loja, itens);
+    }
+    public boolean confirmarPagamento(Pedido pedido, Date dataAtual) {
+        return !dataAtual.after(pedido.getDataVencimentoReserva());
+    }
+}
