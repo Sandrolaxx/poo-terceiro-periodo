d8dd8506d3535658670fd864f542089dc8a941d7
Crediane <csiqueira@minha.fag.edu.br>
2024-09-24 22:39:39 -0300

    Lista sete

---
+++ CREDIANE_SIQUEIRA/primeirob/aulas/listas/listasete/Gerente.java
@@ -0,0 +1,103 @@
+package CREDIANE_SIQUEIRA.primeirob.aulas.listas.listasete;
+
+import java.util.Arrays;
+import java.util.List;
+
+    public class Gerente {
+    private String nome;
+    private int idade;
+    private String loja;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private double salarioBase;
+    private List<Double> salarioRecebido;
+
+    public Gerente(String nome, int idade, String loja, String cidade, String bairro, String rua, double salarioBase) {
+        this.nome = nome;
+        this.idade = idade;
+        this.loja = loja;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.salarioBase = salarioBase;
+        this.salarioRecebido = Arrays.asList(3000.0, 3200.0, 3500.0);
+    }
+
+    // Getters e Setters para encapsulamento
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getLoja() {
+        return loja;
+    }
+
+    public void setLoja(String loja) {
+        this.loja = loja;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public List<Double> getSalarioRecebido() {
+        return salarioRecebido;
+    }
+
+    public void setSalarioRecebido(List<Double> salarioRecebido) {
+        this.salarioRecebido = salarioRecebido;
+    }
+
+    public void apresentarSe() {
+        System.out.println("Nome: " + nome + ", Idade: " + idade + ", Loja: " + loja);
+    }
+
+    public double calcularMedia() {
+        return salarioRecebido.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+    }
+
+    public double calcularBonus() {
+        return salarioBase * 0.35;
+    }
+}
