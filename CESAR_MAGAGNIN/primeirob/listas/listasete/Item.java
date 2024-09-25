71bc568a5d95f356330e1342e5aced4ef4debdb6
cmagagnin <cmagagnin@minha.fag.edu.br>
2024-09-25 02:20:14 -0300

    Versão 4.0 - Lista 7

---
+++ CESAR_MAGAGNIN/primeirob/listas/listasete/Item.java
@@ -0,0 +1,54 @@
+package CESAR_MAGAGNIN.primeirob.listas.listasete;
+import java.io.Serializable;
+
+public class Item implements Serializable {
+    private int id;
+    private String nome;
+    private String tipo;
+    private double valor;
+
+    public Item(int id, String nome, String tipo, double valor) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    // Getters e Setters para encapsulamento
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [id=" + id + ", nome=" + nome + ", tipo=" + tipo + ", valor=" + valor + "]";
+    }
+}
---
+++ CESAR_MAGAGNIN/primeirob/listas/listasete/Loja.java
@@ -0,0 +1,118 @@
+
+package CESAR_MAGAGNIN.primeirob.listas.listasete;
+// Loja.java
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loja implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String cnpj;
+    private Endereco endereco;
+    private List<Funcionario> funcionarios; // Lista de funcionários
+    private List<Cliente> clientes;         // Lista de clientes
+
+    // Construtor
+    public Loja(String nomeFantasia, String razaoSocial, String cnpj, Endereco endereco) {
+        this.nomeFantasia = nomeFantasia;
+        this.razaoSocial = razaoSocial;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.funcionarios = new ArrayList<>();
+        this.clientes = new ArrayList<>();
+    }
+
+    // Métodos para adicionar funcionários e clientes
+    public void adicionarFuncionario(Funcionario funcionario) {
+        if (funcionario != null) {
+            funcionarios.add(funcionario);
+            funcionario.setLoja(this); // Atualiza a referência da loja no funcionário
+        }
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        if (cliente != null) {
+            clientes.add(cliente);
+        }
+    }
+
+    // Método para apresentar-se
+    public void apresentarSe() {
+        System.out.println("Nome Fantasia: " + nomeFantasia);
+        System.out.println("Razão Social: " + razaoSocial);
+        System.out.println("CNPJ: " + cnpj);
+        if (endereco != null) {
+            endereco.apresentarLogradouro();
+        }
+        System.out.println("Número de Funcionários: " + funcionarios.size());
+        System.out.println("Número de Clientes: " + clientes.size());
+    }
+
+    // Métodos para contar clientes e funcionários
+    public int contarClientes() {
+        return clientes.size();
+    }
+
+    public int contarFuncionarios() {
+        return funcionarios.size();
+    }
+
+    // Getters e Setters
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    // Método toString (opcional)
+    @Override
+    public String toString() {
+        return "Loja{" +
+               "nomeFantasia='" + nomeFantasia + '\'' +
+               ", razaoSocial='" + razaoSocial + '\'' +
+               ", cnpj='" + cnpj + '\'' +
+               ", endereco=" + endereco +
+               ", funcionarios=" + funcionarios.size() +
+               ", clientes=" + clientes.size() +
+               '}';
+    }
+}
