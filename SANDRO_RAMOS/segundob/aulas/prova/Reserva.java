09cb40ed7c98cfd93ef0dc91cc1cd71e4c105241
Sandrolaxx <sandro--ramos@hotmail.com>
2024-10-02 12:55:46 -0300

    add revisão prova

---
+++ SANDRO_RAMOS/segundob/aulas/prova/Reserva.java
@@ -0,0 +1,87 @@
+package segundob.aulas.prova;
+
+import java.util.Date;
+
+public class Reserva {
+
+    private Integer id;
+
+    private Cliente cliente;
+
+    private Quarto quarto;
+
+    private Date dataEntrada;
+    
+    private Date dataSaida;
+
+    private Date checkIn;
+    
+    private Date checkOut;
+
+    private Double total;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(Date dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public Date getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(Date dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+    
+}
