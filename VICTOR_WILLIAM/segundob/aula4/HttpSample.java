1613a45021b0f557dfa076f62cf3da9f0cd51e5c
villuntos-skafikis <prawn.staging-0x@icloud.com>
2024-11-07 21:25:27 -0300

    :construction: Aula 4 finalizada

--- VICTOR_WILLIAM/segundob/aula4/HttpSample.java
+++ VICTOR_WILLIAM/segundob/aula4/HttpSample.java
@@ -4,9 +4,13 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
-
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpResponse.BodySubscriber;
 
 public class HttpSample {
 
@@ -14,23 +18,54 @@ public class HttpSample {
 
         String moeda = JOptionPane.showInputDialog(
             null,
-            "Digite a moeda que deseja consultar: ", 
+            "Digite a moeda que deseja consultar (USD, EUR, BTC, etc):", 
             "Cotação de Moedas", 
-            JOptionPane.QUESTION_MESSAGE);
+            JOptionPane.INFORMATION_MESSAGE);
 
-        // Nosso cliente http, precisamos passar a URL
+            String jsonRespostaCotacao = getValorCotacao(moeda);
+
+            Map <String, String> mapJson = getChaveValor(jsonRespostaCotacao);
+
+            JOptionPane.showMessageDialog(
+                null, 
+                mapJson.get("bid"), 
+                "Valor Moeda " + moeda, 0);
+    }
+
+    public static String getValorCotacao(String moeda) throws URISyntaxException, IOException, InterruptedException {
+        
+        // Nosso cliente HTTP, que faz a requisição e recebe a resposta
         HttpClient client = HttpClient.newHttpClient();
 
-        // Criamos a URI, que é a URL que queremos acessar
-        URI uri = URI.create("https://economia.awesomeapi.com.br/json/last/" + moeda);
+        // A URI que vamos acessar para obter a cotação da moeda desejada (dólar, euro, etc)
+        URI uri = new URI("https://economia.awesomeapi.com.br/json/last/" + moeda);
 
-        // Criamos a requisição, passando a URI
-        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        // A requisição que vamos fazer para a URI
+        HttpRequest request = HttpRequest.newBuilder(uri)
+            .GET()
+            .build();
+        
+        // A resposta que vamos receber da requisição
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
-        // Enviando a requisição e recebendo a resposta, o método send retorna um HttpResponse
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
 
-        // Imprimindo o corpo da resposta
-        System.out.println(response.body());
+    public static Map<String, String> getChaveValor(String jsonBody) {
+
+        Pattern pattern = Pattern.compile("\\\"(\\w+)\\\":\\\"([^\\\"]+)\\\"");
+        Matcher matcher = pattern.matcher(jsonBody);
+
+        Map<String, String> mapJson = new HashMap<>();
+
+        while (matcher.find()) {
+
+            String chave = matcher.group(1);
+            String valor = matcher.group(2);
+
+            mapJson.put(chave, valor);
+        }
+
+        return mapJson;
     }
 }
