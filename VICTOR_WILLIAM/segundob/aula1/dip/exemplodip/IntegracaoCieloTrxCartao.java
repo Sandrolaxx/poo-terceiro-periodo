b454d15bf8c2cdea4e3b1cf5e7b44b3fafe76bc9
villuntos-skafikis <prawn.staging-0x@icloud.com>
2024-11-25 02:41:59 -0300

    :recycle: Refactor classes

--- VICTOR_WILLIAM/segundob/aula1/dip/exemplodip/IntegracaoCieloTrxCartao.java
+++ VICTOR_WILLIAM/segundob/aula1/dip/exemplodip/IntegracaoCieloTrxCartao.java
@@ -2,6 +2,8 @@ public class IntegracaoCieloTrxCartao implements IntegracaoAdquirente {
 
     public TransacaoCartao realizarTransacaoCartao(TransacaoCartao trx) {
         System.out.println("Executando Transacao na Cielo");
-        trx.setNsu("222204444")
+        trx.setNsu("222203333")
     }
+
+    return trx;
 }
